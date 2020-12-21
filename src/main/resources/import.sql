576f2f6ebc04473a757512b4ba5b020a7d878180
piprap <57390383+piprap@users.noreply.github.com>
2020-12-21 04:46:44 -0800

    save3

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO Teacher (name) VALUES ('Claus')
+INSERT INTO Teacher (name) VALUES ('Oskar')
+INSERT INTO Teacher (name) VALUES ('Hardy')
+
+
+INSERT INTO Student (name, email, teacher_id) VALUES ('Ole Jensen', 'OJ@gmail.com', 1)
+INSERT INTO Student (name, email, teacher_id) VALUES ('Hanne-lise', 'hanne@gmail.com', 1)
+INSERT INTO Student (name, email, teacher_id) VALUES ('Mark Jeps', 'mark42@gmail.com', 3)
+INSERT INTO Student (name, email, teacher_id) VALUES ('Emil Ostern', 'emil@gmail.com', 2)
+INSERT INTO Student (name, email, teacher_id) VALUES ('Lone Skov', 'LS@gmail.com', 1)
+INSERT INTO Student (name, email, teacher_id) VALUES ('Random rand', 'randy@gmail.com', 1)
